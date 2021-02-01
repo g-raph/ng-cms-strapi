703b8f7b9aae0fd28d056d467768ba6e289bb977
Gielen Gunter <Gunter.Gielen@cegeka.com>
2021-02-01 18:25:46 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9448ffc1883c7270acbeb8f7176d47fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9270c22a-7255-42a5-8c1c-26577586dd9b'
+};
